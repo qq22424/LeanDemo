d6200fbfdfa984ee81730b199d1abdd6b1c202b6
656832739@qq.com <656832739@qq.com>
2017-08-22 21:48:17 +0800

    1.注册

--- public/js/register.js
+++ public/js/register.js
@@ -1,63 +1,56 @@
 $(function(){
-  
-function submit(){
-  // 新建 AVUser 对象实例
-  var user = new AV.User();
-  // 设置用户名
-  user.setUsername($("#username").val());
-  // 设置密码
-  user.setPassword($("#password").val());
-  user.setMobilePhoneNumber($("#username").val());
-  // user.set('mobilePhoneVerified',"true");
-  user.signUp().then(function (loginedUser) {
 
-        $("#mobileVerBox").removeClass("hideBox");
-        $("#submit").addClass("disabled");
-    
-   }, function (error) {
-    alert(error.message);
-  });
-}
-//点击提交注册
-$("#submit").click(function(){
-  submit();  
-  });
-
-// 点击验证手机
-$("#verBtn").click(function(){
-  AV.Cloud.verifySmsCode($("#ver").val(), $("#username").val()).then(function(){
-    alert("验证成功，正在自动寻路~");
-    window.location.href='/';
-}, function(err){
-    alert(err.message);
-});
-});
-
-//  //发送手机号验证码
-//   function verMobile(){
-//      // 新建 AVUser 对象实例
-//   var phoneNumber = $("#username").val();
-//   AV.Cloud.requestSmsCode({
-//     mobilePhoneNumber: phoneNumber,
-//     name: 'Dog单身狗',
-//     op: '领取狗牌',
-//     ttl: 10                     // 验证码有效时间为 10 分钟
-//    }).then(function(){
-//     alert("验证码发送成功")
-//    }, function(err){
-//     //调用失败
-//     alert(err.message);
-//    });
-//   }   
-
-
-
-  $(function () {
-    $('input').iCheck({
-      checkboxClass: 'icheckbox_square-blue',
-      radioClass: 'iradio_square-blue',
-      increaseArea: '20%' // optional
+  $("#submit").click(function(){
+     var user = new AV.User();
+    // 设置用户名
+    user.setUsername($("#username").val());
+    // 设置密码
+    user.setPassword( $("#password").val());
+    user.setMobilePhoneNumber($("#username").val());
+    user.signUp().then(function (loginedUser) { 
+      showCss();
+      alert("狗牌正在运输中，请稍等"); 
+      // verlate()
+     
+    }, function (error) {
+      alert(error.message)
     });
+    
+  });
+  // 验证验证码
+  $("#verBtn").click(function(){
+    AV.User.verifyMobilePhone($("#ver").val()).then(function(){
+      // user.set('mobilePhoneVerified',true);
+      alert("验证通过，自动寻路中~");
+      window.location.href='/';        
+    }, function (error) {
+        // 失败
+        alert(error.message)
+    });          
+  })
+
+// 调用延迟验证
+// function verlate(){
+//   AV.User.requestMobilePhoneVerify($("#username").val()).then(function(){
+//     alert("狗牌正在运输中，请稍等"); 
+// }, function(err){
+//     alert(err.message);
+// });
+// }
+
+ function showCss(){
+    $(".resForm").addClass("hidden");
+    $("#mobileVerBox").removeClass("hidden");
+    $("#submit").addClass("hidden");
+    $("#verBtn").removeClass("hidden");
+  }
+
+  $('input').iCheck({
+    checkboxClass: 'icheckbox_square-blue',
+    radioClass: 'iradio_square-blue',
+    increaseArea: '20%' // optional
   });
 
-  })
+
+});
+
